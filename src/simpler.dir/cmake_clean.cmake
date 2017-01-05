168c7da50a31a21c66c468c989a341ae184a5087
Vasiliy Zhikov <zhikov_n1@mail.ru>
2017-01-05 12:57:59 +0300

    some CV implemented in file simpler.cpp

---
+++ src/simpler.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/../../../src/super_drone/src/simpler.dir/src/simpler.cpp.o"
+  "/home/gand/Documents/ros/drone_project/catkin_ws/devel/lib/super_drone/../../../src/super_drone/src/simpler.pdb"
+  "/home/gand/Documents/ros/drone_project/catkin_ws/devel/lib/super_drone/../../../src/super_drone/src/simpler"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/../../../src/super_drone/src/simpler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
